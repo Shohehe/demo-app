ade473f6d14b73319e6ec8304e4982a8495ae40a
masuyama　shohei <masuyama@3-shake.com>
2019-07-25 00:56:48 +0900

    add img.

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ var PORT = 9898;
 
 // App
 var app = express();
+app.use(express.static(__dirname + '/public'));
 app.get('/', function (req, res) {
-  res.send('Version: 1.1\n');
+  res.send('Version: 2.1<br /><img width="200px" src="apple2.png" />');
 });
 
 app.listen(PORT);
